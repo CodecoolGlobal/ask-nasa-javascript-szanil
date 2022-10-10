e383b51243daf23ef40f610154859b6e7355b7ed
szanil <szaniszlolaszlo95@gmail.com>
2022-10-10 18:07:00 +0200

    Create home.jsx

---
+++ src/routes/home.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import DateSelector from "../components/dateSelector";
+import NasaPicture from "../components/nasaPicture.jsx";
+import DateContext from '../context/dateContext';
+import format from 'date-fns/format';
+
+const Home = () => {
+    const [date, setDate] = useState(format(new Date(Date.now()), 'yyyy-MM-dd'))
+
+    return (
+        <DateContext.Provider value={{ date, setDate }}>
+            <div className="picture-container">
+                <div className="menu">
+                    <p className="menu-text"><h3>Learn about the universe!</h3><br />
+                        <div className="menu-explanation">
+                            Every day we provide a fresh image or video about the cosmos, with a brief explanation from a professional astronomer.
+                        </div>
+                    </p>
+                    <div className="date-picker">
+                        <DateSelector />
+                    </div>
+                </div>
+                <div className="main">
+                    <NasaPicture />
+                </div>
+            </div>
+        </DateContext.Provider>
+    )
+}
+
+export default Home
