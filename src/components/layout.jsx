3c901487caab27c21324e6517235b64fa8c52a85
szanil <szaniszlolaszlo95@gmail.com>
2022-10-11 11:56:38 +0200

    Refactored navbar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
 import NasaLogo from "../assets/Nasa-logo-2.png";
-import NavBar from "./navBar.jsx";
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav className="navbar border-bottom">
+                <div>
+                    <Link className="nav-link text-light" to={'/'}>Home</Link>
+                </div>
+                <div className="logo">
+                    <img src={NasaLogo} style={{ width: "15%" }}></img>
+                </div>
+                <div>
+                    <Link className="nav-link text-light" to={'/'}>Gallery</Link>
+                </div>
+            </nav>
+        </header>
+    );
+}
 
 const Footer = () => {
     return (
