67db73fb315b713ba889e429c44285f45ebc3a81
szanil <szaniszlolaszlo95@gmail.com>
2022-10-11 11:55:36 +0200

    Update nasaApi.js
    Refactored date formatting

--- src/services/nasaApi.js
+++ src/services/nasaApi.js
@@ -1,9 +1,11 @@
+import format from 'date-fns/format';
+
 export const NasaAPI = async (date) => {
     const API_KEY = process.env.REACT_APP_API_KEY;
-    let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`;
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${format(date, 'yyyy-MM-dd')}`;
     try {
         const response = await fetch(url);
-        return response.json()
+        return response.json();
     }
     catch {
         throw new Error('Something went wrong!')
